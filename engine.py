89c27255c1f33363dbd6967d739bb493919c0bb2
sergiorf <sergio.rodriguez.freire@gmail.com>
2020-01-11 01:10:36 +0100

    test

--- engine.py
+++ engine.py
@@ -1,9 +1,24 @@
+import nltk
 from nltk import load_parser
+#queries:
+#what are the 10 largest shareholders of sony
+#what companies whent public in belgium from 2017 to 2019
+#give me 20 shipping companies worldwide ordered by sales 
+#give me all foreign companies that have operations in poland
 
-sent = 'who do you like'
-tokens = sent.split()
-cp = load_parser('feat1.fcfg')
+#preprocessing:
+#replace all verb tenses by root 
+#replace names by placeholder 
+
+sent = 'what are the 10 shareholders'
+original_sent = sent.split()
+
+# CFGs doens't handle multidigit numbers so we compress it before
+tokens = ['#NUM#' if i.isdigit() else i for i in original_sent]
+
+cp = load_parser('hal.fcfg')
 trees = cp.parse(tokens)
 for tree in trees:
+    print (tree)
     tree.draw()
 
