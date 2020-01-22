8fd24dbe48aeb27f39c33f2606ab80bf6bbe9cbe
sergiorf <sergio.rodriguez.freire@gmail.com>
2020-01-23 00:02:53 +0100

    test

--- hal_expression.py
+++ hal_expression.py
@@ -7,8 +7,14 @@ class HalExpression:
 
         self.tree = tree
         self.table_metadata = {
-            "shareholder": ["id", "name", { "of": ["company", "company_id"] }]
-            "company": ["id", "name"]
+            "shareholder_company": { 
+                "id":"id", 
+                "shareholder_id":"shareholder_id",
+                "company_id":"company_id",
+                "large": "participation" 
+            },
+            "shareholder": { "id":"id", "name":"name" },
+            "company": { "id":"id", "name":"name" }
         }
 
     def gen_sql(self):
@@ -17,12 +23,12 @@ class HalExpression:
             all(isinstance(x, FeatStructNonterminal) for x in tree)):
             raise ValueError('Invalid sentence: a sentence is composed of VP and NP')
 
-        sent_struct = [repr(tree[i].label()) for i in range(2)]
+        sent_struct = [repr(self.tree[i].label()) for i in range(2)]
         sql_tokens = []
         if (['NP[]', 'VP[]'] == sent_struct):
-            self.gen_npfirst_sent(tree[0], tree[1], sql_tokens)
+            self.gen_npfirst_sent(self.tree[0], self.tree[1], sql_tokens)
         elif (['VP[]', 'NP[]'] == sent_struct):
-            self.gen_vpfirst_sent(tree[0], tree[1], sql_tokens)
+            self.gen_vpfirst_sent(self.tree[0], self.tree[1], sql_tokens)
         else:
             raise ValueError('Invalid sentence structure: a sentence is a NP followed by VP or viceversa')
         
@@ -46,28 +52,20 @@ class HalExpression:
 
     def get_form1(self, leaves, sql_tokens):
 
-        num, name1, _, name2 = self.grep_leaves(leaves, 'NUM[]*N[]*P[]*N[]')
+        num, adj, name1, _, name2 = self.grep_leaves(leaves, 'NUM[]*Adj[]*N[]*P[]*N[]')
+        if not name1 or not name2:
+            num, name1, _, name2 = self.grep_leaves(leaves, 'NUM[]*N[]*P[]*N[]')
         if num and name1 and name2:
-            if name1 in self.table_metadata:
-
-                sql_tokens.append'top(' + num + ')')
-                sql_tokens.append('*')
-                sql_tokens.append('from')
-                sql_tokens.append(name1 + ' as n1')
-                sql_tokens.append('inner join')
-                sql_tokens.append(self.table_metadata["of"][0])
-                sql_tokens.append("as n2")
-                sql_tokens.append("on n2.id = n1." + self.table_metadata["of"[1]])
-                sql_tokens.append("where n2.name = " + name2)                
-                return True
-
+            sql_tokens.append('top(' + num + ')')
+            sql_tokens.append('*')
+            sql_tokens.append('from shareholder_company as n1')
+            sql_tokens.append('inner join shareholder_company as n2 on n2.id = n1.shareholder_id')
+            sql_tokens.append('inner join company as n3 on n3.id = n3.company_id')                
+            sql_tokens.append("where n3.name = \"" + name2 + "\"")                
+            if adj:
+                sql_tokens.append("order by n2.participation desc")
+            return True
         return False
-
-#"select top(10) * from shareholder as sh " \
-#                  "join company as co on co.id = sh.company_id "  \
-#                  "where co.name = \"sony\"" \
-#                   )
-        return False 
 
     def grep_leaves(self, leaves, regex):
         i = 0
@@ -93,7 +91,7 @@ class HalExpression:
         return leaves
     
 if __name__ == '__main__':
-    tree = HalTreeParser().get_tree(query = "what are the 10 shareholders of sony")
+    tree = HalTreeParser().get_tree(query = "what are the 10 largest shareholders of sony")
     print (HalTreeParser().get_pprint(tree))
     expr = HalExpression(tree)
     print (expr.gen_sql())
--- hal_expressions_tests.py
+++ hal_expressions_tests.py
@@ -6,18 +6,29 @@ from hal_expression import HalExpression
 class HalExpressionsTests(unittest.TestCase):
 
     def test_expressions_1(self):
-        self.validate_sql(query = "what are the 10 shareholders of sony",
-            expected_sql = "select top(10) * from shareholder as sh " \
-                  "join company as co on co.id = sh.company_id "  \
-                  "where co.name = \"sony\"" \
-                   )
+        query = "what are the 10 shareholders of sony"
+        expected_sql = "SELECT top(10) * from shareholder_company as n1 " \
+                "inner join shareholder_company as n2 on n2.id = n1.shareholder_id " \
+                "inner join company as n3 on n3.id = n3.company_id " \
+                "where n3.name = \"sony\""
+        self.validate_sql(query, expected_sql)
+
+    def test_expressions_2(self):
+        query = "what are the 10 largest shareholders of sony"
+        expected_sql = "SELECT top(10) * from shareholder_company as n1 " \
+                "inner join shareholder_company as n2 on n2.id = n1.shareholder_id " \
+                "inner join company as n3 on n3.id = n3.company_id " \
+                "where n3.name = \"sony\" order by n2.participation desc"
+        self.validate_sql(query, expected_sql)
 
     def validate_sql(self, query, expected_sql):
-        tree = HalTreeParser().get_tree(query)
-        gen_sql = HalExpression(tree).gen_sql()
-        print ('query: ' + query)
-        print ('expected sql: ' + expected_sql)
-        print ('gen sql: ' + gen_sql)
+        parser = HalTreeParser()
+        tree = parser.get_tree(query)
+        expr = HalExpression(tree)
+        gen_sql = expr.gen_sql()
+        print ('query:        \"' + query + "\"")
+        print ('expected sql: \"' + expected_sql + "\"")
+        print ('gen sql:      \"' + gen_sql + "\"")
         self.assertEqual(expected_sql, gen_sql, "Invalid sql")
 
 if __name__ == '__main__':
